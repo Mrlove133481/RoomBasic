a24c2912d449bdd1d98cf01dc7c7390868e853bd
Mrlove133481 <1334819077@qq.com>
2020-01-17 13:54:56 +0800

    数据插入成功

--- app/src/main/java/com/mrlove/roombasic/MainActivity.java
+++ app/src/main/java/com/mrlove/roombasic/MainActivity.java
@@ -18,8 +18,7 @@ import com.mrlove.roombasic.domain.Word;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String LAG = "mrlove_str";
-    MyViewModel myViewModel;
+     MyViewModel myViewModel;
      ActivityMainBinding binding;
      WordDao wordDao;
      WordDataBase wordDataBase;
@@ -33,8 +32,6 @@ public class MainActivity extends AppCompatActivity {
                 .allowMainThreadQueries()
                 .build();
         wordDao = wordDataBase.getWordDao();
-       binding.textView2.setText("你is的广泛的非官方大哥\n但是梵蒂冈梵蒂冈和开发\n的方式的开发工具或多个\n你is的广泛的非官方大哥\n但是梵蒂冈梵蒂冈和开发\n的方式的开发工具或多个\n你is的广泛的非官方大哥\n但是梵蒂冈梵蒂冈和开发\n的方式的开发工具或多个\n你is的广泛的非官方大哥\n但是梵蒂冈梵蒂冈和开发\n的方式的开发工具或多个\n你is的广泛的非官方大哥\n但是梵蒂冈梵蒂冈和开发\n的方式的开发工具或多个\n");
-        Log.d(LAG,binding.textView2.getText().toString());
         updateView();
 
         binding.buttoninsert.setOnClickListener(new View.OnClickListener() {
@@ -43,23 +40,18 @@ public class MainActivity extends AppCompatActivity {
                 Word word1 = new Word("hello","你好");
                 Word word2 = new Word("world","世界");
                 wordDao.insertWords(word1,word2);
-                Log.d(LAG,"aaa");
                 updateView();
             }
         });
-        binding.setLifecycleOwner(this);
     }
 
     void updateView(){
-        Log.d(LAG,"bbb");
         List<Word> list = wordDao.getAllWords();
-        Log.d(LAG,list.toString());
         String str = "";
         for (Word word: list
              ) {
             str += word.getId()+":"+word.getWord()+"="+word.getChineseMeaning()+"\n";
         }
-        Log.d(LAG,str);
         binding.textView2.setText(str);
     }
 
